61de1cf10dcee655c83df5f422aeb8d196c9f5f1
Pawel Jakubowski <p.m.jakubowski@gmail.com>
2016-08-16 20:38:25 +0200

    test: refactor and add missing tests

--- test/yadic.test.js
+++ test/yadic.test.js
@@ -5,107 +5,194 @@ var path = require('path');
 var Yadic = require('../yadic');
 
 describe('yadic', function() {
-    var yadic;
    
-    beforeEach(function() {
-        yadic = new Yadic({
-            constructorModule: path.join(__dirname, './modules/constructor-module'),
-            factoryModule: path.join(__dirname, './modules/factory-module'),
-            plainModule: path.join(__dirname, './modules/plain-module'),
-            singletonModule: path.join(__dirname, './modules/singleton-module')
-        });
-    });
-
-    it('should create module from constructor', function() {
-        var Constructor = require('./modules/constructor-module');
-
-        return yadic.get('constructorModule').then(function(mod) {
-            expect(mod instanceof Constructor).toBe(true);
-        });
-    });
-
-    it('should create different module from constructor for each call', function() {
-        return yadic.get('constructorModule').then(function(modA) {
-            return yadic.get('constructorModule').then(function(modB) {
-                expect(modA).not.toBe(modB);
-            });
-        });
-    });
-
-    it('should create module from factory', function() {
-        return yadic.get('factoryModule').then(function(mod) {
-            expect(mod).toEqual({name:'instance'});
-        });
-    });
-
-    it('should create different module from factory for each call', function() {
-        return yadic.get('factoryModule').then(function(modA) {
-            return yadic.get('factoryModule').then(function(modB) {
-                expect(modA).not.toBe(modB);
-            });
-        });
-    });
-
-    it('should return plain module', function() {
-        var plainModule = require('./modules/plain-module');
-        return yadic.get('plainModule').then(function(mod) {
-            expect(plainModule).toBe(mod);
-        });
-    });
-
-    it('should return a singleton', function() {
-        return yadic.get('singletonModule').then(function(modA) {
-            return yadic.get('singletonModule').then(function(modB) {
-                expect(modA).toBe(modB);
-            });
-        });
-    });
-
-    it('should return module with injected dependencies', function() {
-        var ConstructorWithDependencies = jest.fn();
-        ConstructorWithDependencies['@type'] = 'constructor';
-        ConstructorWithDependencies['@inject'] = ['plainModule', 'constructorModule'];
-
-        yadic.addFactories({
-            'moduleWithDependencies': ConstructorWithDependencies
-        });
-
-        return yadic.get('moduleWithDependencies').then(function(mod) {
-            var plainModule = require('./modules/plain-module');
-            var Constructor = require('./modules/constructor-module');
-
-            expect(mod instanceof ConstructorWithDependencies).toBe(true);
-            expect(ConstructorWithDependencies.mock.calls[0][0]).toBe(plainModule);
-            expect(ConstructorWithDependencies.mock.calls[0][1] instanceof Constructor).toBe(true);
-        });
-    });
-
-    it('should throw an error for invalid path', function() {
-        expect(function() {
+    describe('constructor', function() {
+        
+        it('should load modules from path', function() {
             new Yadic({
-                module: './a/b/c'
+                constructorModule: path.join(__dirname, './modules/constructor-module'),
+                factoryModule: path.join(__dirname, './modules/factory-module'),
+                plainModule: path.join(__dirname, './modules/plain-module'),
+                singletonModule: path.join(__dirname, './modules/singleton-module')
             });
-        }).toThrow();
+        });
+
+        it('should load plain modules', function() {
+            new Yadic({
+                functionModule: function() {},
+                objectModule: {}
+            });
+        });
+
+        it('should throw an error for invalid path', function() {
+            expect(function() {
+                new Yadic({
+                    module: './a/b/c'
+                });
+            }).toThrow();
+        });
+
     });
 
-    it('should reject for unknown module', function() {
-        return yadic.get('unknown-module').catch(function (error) {
-            expect(error instanceof Error).toBe(true);
+    describe('#addFactories', function() {
+
+        it('should load modules from path', function() {
+            var yadic = new Yadic();
+
+            yadic.addFactories({
+                constructorModule: path.join(__dirname, './modules/constructor-module'),
+                factoryModule: path.join(__dirname, './modules/factory-module'),
+                plainModule: path.join(__dirname, './modules/plain-module'),
+                singletonModule: path.join(__dirname, './modules/singleton-module')
+            });
         });
+
+        it('should load plain modules', function() {
+            var yadic = new Yadic();
+
+            yadic.addFactories({
+                functionModule: function() {},
+                objectModule: {}
+            });
+        });
+
     });
 
-    it('should reject for unknown dependencies to inject', function() {
-        var ConstructorWithDependencies = jest.fn();
-        ConstructorWithDependencies['@type'] = 'constructor';
-        ConstructorWithDependencies['@inject'] = ['unknownDependency'];
+    describe('#get', function() {
 
-        yadic.addFactories({
-            'moduleWithDependencies': ConstructorWithDependencies
+        it('should create module from constructor when @type is `constructor`', function() {
+            var Constructor = function() {};
+            Constructor['@type'] = 'constructor';
+
+            var yadic = new Yadic({
+                constructorModule: Constructor
+            });
+
+            return yadic.get('constructorModule').then(function(mod) {
+                expect(mod instanceof Constructor).toBe(true);
+            });
         });
 
-        return yadic.get('moduleWithDependencies').catch(function (error) {
-            expect(error instanceof Error).toBe(true);
+        it('should create different module from constructor for each call', function() {
+            var Constructor = function() {};
+            Constructor['@type'] = 'constructor';
+
+            var yadic = new Yadic({
+                constructorModule: Constructor
+            });
+
+            return yadic.get('constructorModule').then(function(modA) {
+                return yadic.get('constructorModule').then(function(modB) {
+                    expect(modA).not.toBe(modB);
+                });
+            });
         });
+
+        it('should create module from factory when @type is factory', function() {
+            var factory = jest.fn(function() {
+                return {name: 'instance'};
+            });
+            factory['@type'] = 'factory';
+
+            var yadic = new Yadic({
+                factoryModule: factory
+            });
+
+            return yadic.get('factoryModule').then(function(mod) {
+                expect(mod).toEqual({name:'instance'});
+            });
+        });
+
+        it('should create different module from factory for each call', function() {
+            var factory = jest.fn(function() {
+                return {name: 'instance'};
+            });
+            factory['@type'] = 'factory';
+
+            var yadic = new Yadic({
+                factoryModule: factory
+            });
+
+            return yadic.get('factoryModule').then(function(modA) {
+                return yadic.get('factoryModule').then(function(modB) {
+                    expect(modA).not.toBe(modB);
+                });
+            });
+        });
+
+        it('should return a singleton', function() {
+            var Singleton = function() {
+                return {};
+            };
+            Singleton['@type'] = 'constructor';
+            Singleton['@singleton'] = true;
+
+            var yadic = new Yadic({
+                singletonModule: Singleton
+            });
+
+            return yadic.get('singletonModule').then(function(modA) {
+                return yadic.get('singletonModule').then(function(modB) {
+                    expect(modA).toBe(modB);
+                });
+            });
+        });
+
+        it('should return plain module', function() {
+            var plain = {};
+            var yadic = new Yadic({
+                plainModule: plain
+            });
+
+            return yadic.get('plainModule').then(function(mod) {
+                expect(mod).toBe(plain);
+            });
+        });
+
+        it('should return module with injected dependencies', function() {
+            var plain = {};
+            var Constructor = function() {};
+            Constructor['@type'] = 'constructor';
+
+            var ConstructorWithDependencies = jest.fn();
+            ConstructorWithDependencies['@type'] = 'constructor';
+            ConstructorWithDependencies['@inject'] = ['plainModule', 'constructorModule'];
+
+            var yadic = new Yadic({
+                constructorModule: Constructor,
+                plainModule: plain,
+                constructorWithDependencies: ConstructorWithDependencies
+            });
+
+            return yadic.get('constructorWithDependencies').then(function(mod) {
+                expect(mod instanceof ConstructorWithDependencies).toBe(true);
+                expect(ConstructorWithDependencies.mock.calls[0][0]).toBe(plain);
+                expect(ConstructorWithDependencies.mock.calls[0][1] instanceof Constructor).toBe(true);
+            });
+        });
+
+        it('should reject for unknown module', function() {
+            var yadic = new Yadic({});
+
+            return yadic.get('unknown-module').catch(function(error) {
+                expect(error instanceof Error).toBe(true);
+            });
+        });
+
+        it('should reject for unknown dependencies to inject', function() {
+            var ConstructorWithDependencies = jest.fn();
+            ConstructorWithDependencies['@type'] = 'constructor';
+            ConstructorWithDependencies['@inject'] = ['unknownDependency'];
+
+            var yadic = new Yadic({
+                constructorWithDependencies: ConstructorWithDependencies
+            });
+
+            return yadic.get('constructorWithDependencies').catch(function(error) {
+                expect(error instanceof Error).toBe(true);
+            });
+        });
+
     });
 
 });
